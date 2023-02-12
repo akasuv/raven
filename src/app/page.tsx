e94eeec8e2d5a901e7d3727664cc7059e9e5c272
Wei Su <wei@funxociety.com>
2023-02-12 22:56:42 +0800

    update logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,17 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div style={{ display: "flex", gap: 10, flexWrap: "wrap" }}>Raven</div>
+      <div
+        style={{
+          display: "flex",
+          gap: 10,
+          flexWrap: "wrap",
+          height: 300,
+          width: 300,
+        }}
+      >
+        <img src="/rvn.png" width="100%" height="100%" />
+      </div>
     </main>
   );
 }
