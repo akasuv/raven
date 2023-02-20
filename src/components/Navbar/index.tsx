b6afb8e07b31b805355bdef5f2f62d442bf96e91
Wei Su <me@weisu.io>
2023-02-21 00:23:24 +0800

    test logo quality

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Navbar = () => (
   <nav className="py-4 px-5 md:px-10 lg:px-12 flex items-center justify-between">
     <div className="flex gap-x-5 items-center">
-      <Image src="/logo.svg" alt="Raven" width={36} height={36} />
+      <img src="/logo.svg" alt="Raven" width={36} height={36} />
       <p className="text-white font-bold">Raven Studio</p>
     </div>
     <ul className="text-[#989898]">
