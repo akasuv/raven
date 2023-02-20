39df59b4060bc811938addbd834409c105ed2728
Wei Su <me@weisu.io>
2023-02-21 00:25:47 +0800

    test logo

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Navbar = () => (
   <nav className="py-4 px-5 md:px-10 lg:px-12 flex items-center justify-between">
     <div className="flex gap-x-5 items-center">
-      <img src="/logo.svg" alt="Raven" width={36} height={36} />
+      <Image src="/logo.png" alt="Raven" width={36} height={36} />
       <p className="text-white font-bold">Raven Studio</p>
     </div>
     <ul className="text-[#989898]">
